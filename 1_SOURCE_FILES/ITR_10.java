f714751d2463e113f8d9bcfe1d349a40a12548f6
Dmitry358 <Dmitry358@github.com>
2023-05-16 22:24:23 +0200

    !!! FINITI ITR_10 E ITRTEST_10 !!!

--- 1_SOURCE_FILES/ITR_10.java
+++ 1_SOURCE_FILES/ITR_10.java
@@ -19,6 +19,11 @@ public class IntegerToRoman{
         else if(number == 4) { result = "IV"; }
         else if(number == 5) { result = "V"; }
         else if(number == 6) { result = "VI"; }
+        else if(number == 7) { result = "VII"; }
+        else if(number == 8) { result = "VIII"; }
+        else if(number == 9) { result = "IX"; }
+        else if(number == 10) { result = "X"; }
+
         return result;
     }
 
