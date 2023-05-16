a103b56d9aa69d3c313d0c049c1212966d3619dc
Dmitry358 <Dmitry358@github.com>
2023-05-16 21:50:35 +0200

    !!! REALIZZATO SCRIPT VERS 1-10

--- 1_SOURCE_FILES/ITR_10.java
+++ 1_SOURCE_FILES/ITR_10.java
@@ -3,47 +3,24 @@
 // Dmitry Pluzhnikov 1169886
 ////////////////////////////////////////////////////////////////////
 
+////////////////////   V-10   ////////////////////
+
 package it.unipd.mtss;
 
-import java.io.IOException; //TEMPORANEO
-import java.util.Scanner;
-import java.io.File; //TEMPORANEO
-import java.io.PrintWriter; //TEMPORANEO
+import java.lang.String;
 
 public class IntegerToRoman{
 
-    public static void main( String[] args ) {
-
-        stampaInFile();
-        Scanner in = new Scanner(System.in);
-        System.out.print("\nINSERISCI NUMERO: ");
-        int num = in.nextInt();
-        in.close();
-
-        numUni(num);
-        System.out.print("\n");
+    public static String convert(int number){
+        String result = null;
+        if(number == 1) { result = "I"; }
+        else if(number == 2) { result = "II"; }
+        else if(number == 3) { result = "III"; }
+        else if(number == 4) { result = "IV"; }
+        else if(number == 5) { result = "V"; }
+        else if(number == 6) { result = "VI"; }
+        return result;
     }
-	
-	public static String convert(int number){
-        // TODO
-        return null;
-    }
 
-//!!!!!! REALIZZARE FUNZIONE DI CONTROLLO DI CORRETTEZZA DI INPUT
 
-    public static void numUni(int num_uni){
-        //System.out.printf("  Uni:");
-        switch (num_uni){
-            case (1): System.out.printf("I"); break;
-            case (2): System.out.printf("II"); break;
-            case (3): System.out.printf("III"); break;
-            case (4): System.out.printf("IV"); break;
-            case (5): System.out.printf("V"); break;
-            case (6): System.out.printf("VI"); break;
-            case (7): System.out.printf("VII"); break;
-            case (8): System.out.printf("VIII"); break;
-            case (9): System.out.printf("IX"); break;
-			case (10): System.out.printf("X"); break;
-        }
-    }
 }
