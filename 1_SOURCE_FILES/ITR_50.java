3099cdae61b0e2fc4358aab8511f936709d238b4
Dmitry358 <Dmitry358@github.com>
2023-05-17 10:00:28 +0200

    SEMBRANO CORRETTI ITR_50 E ITR_50_TEST

--- 1_SOURCE_FILES/ITR_50.java
+++ 1_SOURCE_FILES/ITR_50.java
@@ -3,93 +3,89 @@
 // Dmitry Pluzhnikov 1169886
 ////////////////////////////////////////////////////////////////////
 
+////////////////////   V-50   ////////////////////
+
 package it.unipd.mtss;
 
-import java.io.IOException; //TEMPORANEO
-import java.util.Scanner;
-import java.io.File; //TEMPORANEO
-import java.io.PrintWriter; //TEMPORANEO
+import java.lang.String;
 
 public class IntegerToRoman{
 
-    public static void main( String[] args ) {
-
-        stampaInFile();
-        Scanner in = new Scanner(System.in);
-        System.out.print("\nINSERISCI NUMERO: ");
-        int num = in.nextInt();
-        in.close();
-
-        numDec(num);
-        System.out.print("\n");
-    }
-	
-	public static String convert(int number){
-        // TODO
-        return null;
-    }
-
-//!!!!!! REALIZZARE FUNZIONE DI CONTROLLO DI CORRETTEZZA DI INPUT
-
-    public static void numDec(int num_dec){
-
-         // num_dec == 50
-         if(num_dec = 50) System.out.printf("L");
-
-         // 49 >= num_dec >= 10
-         else if(num_dec/10>=1){
-             
-			 // 49 >= num >= 40
-             if (num_dec / 40 >= 1) {
-                 System.out.printf("XL");
-                 numUni(num_dec - 40);
-             }
-
-             // 39 >= num >= 10
-             else {
-                 for (int i = 1; i <= num_dec / 10; i++) System.out.printf("X");
-                 numUni(num_dec % 10);
-             }
-         }
-
-         else numUni(num_dec);
-    }
-
-    public static void numUni(int num_uni){
-        //System.out.printf("  Uni:");
-        switch (num_uni){
-            case (1): System.out.printf("I"); break;
-            case (2): System.out.printf("II"); break;
-            case (3): System.out.printf("III"); break;
-            case (4): System.out.printf("IV"); break;
-            case (5): System.out.printf("V"); break;
-            case (6): System.out.printf("VI"); break;
-            case (7): System.out.printf("VII"); break;
-            case (8): System.out.printf("VIII"); break;
-            case (9): System.out.printf("IX"); break;
+    protected static String stampaDaUnoATre(int number){
+        String result = "";
+        for(int i = 1; i <= number; i++){
+            result += "I";
         }
+        return  result;
     }
 
-    public static void stampaInFile() {
-        /*try {
-            File file = new File("C:\\Users\\NADIYA\\Desktop\\all_num.txt");
+    protected static String stampaDaSeiAOtto(int number){
+        String result = "V";
+        for(int i = 1; i <= number-5; i++){
+            result += "I";
+        }
+        return  result;
+    }
 
-            if(!file.exists()) file.createNewFile();
-            PrintWriter pw = new PrintWriter(file);
-        */
-            for (int i = 1; i < 1001; i++) {
-                //pw.println(i);
-                System.out.printf(i+" = ");
-                numCent(i);
-                System.out.printf(" \n");
+    protected static String numUni(int number){
+        String result = "";
+
+        if(number>=1 && number <=3){
+            result = stampaDaUnoATre(number);
+        }
+
+        else if(number == 4) { result = "IV"; }
+        else if(number == 5) { result = "V"; }
+
+        else if(number>=6 && number <=8){
+            result = stampaDaSeiAOtto(number);
+        }
+
+        else if(number == 9) { result = "IX"; }
+
+        return result.isEmpty() ? null : result;
+    }
+
+    public static String convert(int number) {
+        String result = "";
+
+        if(number > 50 || number < 1) { return null; }
+
+        // num_dec == 50
+        else if(number == 50) { return "L";}
+
+        // 49 >= num_dec >= 1
+        else if(number > 0){
+
+            // 49 >= num >= 1
+            if (number / 40 >= 1) {
+                result += "XL";
+                if (number % 10 > 0) {
+                    // 9 >= num >= 1
+                    result += numUni(number % 10);
+                }
             }
-        /*
-            pw.close();
+
+            // 39 >= num >= 1
+            else {
+                // 39 >= num >= 30
+                for (int i = 1; i <= number / 10; i++) {
+                    result += "X";
+                }
+
+                if (number % 10 > 0) {
+                    // 9 >= num >= 1
+                    result += numUni(number % 10);
+                }
+
+            }
         }
 
-        catch (IOException e){
-            System.out.printf("ERRORE CON FILE:" + e);
-        }
-        */
+        // 9 >= num >= 1
+        //result += numUni(number % 10);
+
+
+        return result.isEmpty() ? null : result;
     }
+
 }
