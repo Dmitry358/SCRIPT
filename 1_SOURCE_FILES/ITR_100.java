ba9766bc7594fe6f91f97d44c8efaa930b25bf18
Dmitry358 <Dmitry358@github.com>
2023-05-17 12:57:49 +0200

    !!FATTI IN FRETTA SEMBRANO CORRETTI ITR_100 E ITR_100TEST

--- 1_SOURCE_FILES/ITR_100.java
+++ 1_SOURCE_FILES/ITR_100.java
@@ -3,89 +3,121 @@
 // Dmitry Pluzhnikov 1169886
 ////////////////////////////////////////////////////////////////////
 
+////////////////////   V-100   ////////////////////
+
 package it.unipd.mtss;
 
-import java.io.IOException; //TEMPORANEO
-import java.util.Scanner;
-import java.io.File; //TEMPORANEO
-import java.io.PrintWriter; //TEMPORANEO
+import java.lang.String;
 
 public class IntegerToRoman{
 
-    public static void main( String[] args ) {
-
-        stampaInFile();
-        Scanner in = new Scanner(System.in);
-        System.out.print("\nINSERISCI NUMERO: ");
-        int num = in.nextInt();
-        in.close();
-
-        numCent(num);
-        System.out.print("\n");
-    }
-	
-	public static String convert(int number){
-        // TODO
-        return null;
-    }
-
-//!!!!!! REALIZZARE FUNZIONE DI CONTROLLO DI CORRETTEZZA DI INPUT
-
-    public static void numCent(int num_dec){
-         int num_dec_rest = 0;
-         int num_uni = 0;
-		 
-		 // num_dec == 100
-		 if (num_dec == 100) System.out.printf("C");
-
-         // 99 >= num_dec >= 90
-         else if (num_dec / 90 >= 1){
-            System.out.printf("XC");
-            numUni(num_dec - 90);
-         }
-
-         // 99 >= num_dec >= 50
-         else if(num_dec/50>=1) {
-             System.out.printf("L");
-
-             num_dec_rest = num_dec - 50;
-
-             for (int i = 1; i <= num_dec_rest / 10; i++) System.out.printf("X");
-
-             numUni(num_dec % 10);
-         }
-
-         // 49 >= num_dec >= 10
-         else if(num_dec/10>=1){
-             
-			 // 49 >= num >= 40
-             if (num_dec / 40 >= 1) {
-                 System.out.printf("XL");
-                 numUni(num_dec - 40);
-             }
-
-             // 39 >= num >= 10
-             else {
-                 for (int i = 1; i <= num_dec / 10; i++) System.out.printf("X");
-                 numUni(num_dec % 10);
-             }
-         }
-
-         else numUni(num_dec);
-    }
-
-    public static void numUni(int num_uni){
-        switch (num_uni){
-            case (1): System.out.printf("I"); break;
-            case (2): System.out.printf("II"); break;
-            case (3): System.out.printf("III"); break;
-            case (4): System.out.printf("IV"); break;
-            case (5): System.out.printf("V"); break;
-            case (6): System.out.printf("VI"); break;
-            case (7): System.out.printf("VII"); break;
-            case (8): System.out.printf("VIII"); break;
-            case (9): System.out.printf("IX"); break;
+    protected static String stampaDaUnoATre(int number){
+        String result = "";
+        for(int i = 1; i <= number; i++){
+            result += "I";
         }
+        return  result;
+    }
+
+    protected static String stampaDaSeiAOtto(int number){
+        String result = "V";
+        for(int i = 1; i <= number-5; i++){
+            result += "I";
+        }
+        return  result;
+    }
+
+    protected static String numUni(int number){
+        String result = "";
+
+        if(number>=1 && number <=3){
+            result = stampaDaUnoATre(number);
+        }
+
+        else if(number == 4) { result = "IV"; }
+        else if(number == 5) { result = "V"; }
+
+        else if(number>=6 && number <=8){
+            result = stampaDaSeiAOtto(number);
+        }
+
+        else if(number == 9) { result = "IX"; }
+
+        return result.isEmpty() ? null : result;
+    }
+
+    protected static String stampaDaCentoACinquanta(int number){
+        String result = "";
+
+        // num_dec == 100
+        if(number == 100) { return "C"; }
+
+        // 99 >= num_dec >= 90
+        else if (number / 90 >= 1){
+            result += "XC";
+            if (number % 10 > 0) {
+                // 9 >= num >= 1
+                result += numUni(number % 10);
+            }
+        }
+
+        // 89 >= num >= 1
+        else if(number/51>=1) {
+            result += "L";
+
+            // 49 >= num >= 1
+            for (int i = 1; i <= (number - 50) / 10; i++){
+                result += "X";
+            }
+
+            // 9 >= num >= 1
+            if (number % 10 > 0) {
+                result += numUni(number % 10);
+            }
+        }
+
+        // num_dec == 50
+        else if(number == 50) {
+            return "L";
+        }
+
+        return result;
+    }
+
+    public static String convert(int number) {
+        String result = "";
+        if(number > 100 || number < 1) { return null; }
+
+        result += stampaDaCentoACinquanta(number);
+
+        // 49 >= num_dec >= 1
+        if(number > 0 && number < 50){
+
+            // 49 >= num >= 1
+            if (number / 40 >= 1 ) {
+                result += "XL";
+                if (number % 10 > 0) {
+                    // 9 >= num >= 1
+                    result += numUni(number % 10);
+                }
+            }
+
+            // 39 >= num >= 1
+            else {
+                // 39 >= num >= 30
+                for (int i = 1; i <= number / 10; i++) {
+                    result += "X";
+                }
+
+                if (number % 10 > 0) {
+                    // 9 >= num >= 1
+                    result += numUni(number % 10);
+                }
+            }
+        }
+
+        return result.isEmpty() ? null : result;
     }
 
 }
+
